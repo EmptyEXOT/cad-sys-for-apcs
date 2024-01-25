c829be2b0c9ccd247aa31f2a70a643be6b8d8795
EmptyEXOT <sadullaev.alim@mail.ru>
2024-01-25 20:55:33 +0300

    cover section

--- client/src/shared/Button/Button.tsx
+++ client/src/shared/Button/Button.tsx
@@ -1,14 +1,19 @@
 import React, {ButtonHTMLAttributes, FC, ReactNode} from 'react';
 import classNames from "classnames";
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     children: ReactNode;
     className?: string;
+    border?: boolean;
+    fill? : boolean;
 }
 
 const Button: FC<ButtonProps> = (props) => {
     return (
-        <button {...props} className={classNames('px-2 flex flex-col justify-center', props.className)}>
+        <button {...props}
+                className={classNames('text-2xl p-4 px-5 justify-center', props.className, props.border
+                    ? 'border-black rounded border-solid' : '',
+                    props.fill ? 'bg-black text-white' : '')}>
             {props.children}
         </button>
     );
